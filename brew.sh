fd461ec577e60d1cb25b42508b89bef4cef91e3d
Ken <54856408+ken-365@users.noreply.github.com>
2022-02-22 11:33:04 -0600

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -19,9 +19,9 @@ brew tap homebrew/cask-fonts
 brew cask install font-source-code-pro
 
 
-1. iterm
-2. https://ohmyz.sh/
-3. https://github.com/romkatv/powerlevel10k
-4. clipy-app.com
-5. https://www.spectacleapp.com/
-7. https://github.com/p0deje/Maccy
+# 1. iterm
+# 2. https://ohmyz.sh/
+# 3. https://github.com/romkatv/powerlevel10k
+# 4. clipy-app.com
+# 5. https://www.spectacleapp.com/
+# 7. https://github.com/p0deje/Maccy
