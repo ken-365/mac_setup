6e2c056d557067ef4ba7ba38b62ad27565ff7c28
Ken <54856408+ken-365@users.noreply.github.com>
2022-02-22 11:18:58 -0600

    Create brew.sh
    init

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install vlc
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
