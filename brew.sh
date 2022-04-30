31876d3721abcfa24ab91064ec92d484acfd4078
Ken <54856408+ken-365@users.noreply.github.com>
2022-04-29 20:07:16 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,24 +6,17 @@ brew install tree
 brew install sevenzip
 
 # Install MacOS Applications
+brew install --cask raycast
 brew install --cask google-chrome
 brew install --cask visual-studio-code
 brew install --cask sourcetree
 brew install --cask vlc
-brew install --cask clipy
-brew install --cask rectangle
-brew install --cask maccy
 brew install --cask ticktick
-brew install --cask alfred
 brew install --cask spotify
 brew install --cask sublime-text
 brew install --cask numi
 brew install --cask qbittorrent
-brew install --cask cheatsheet
 brew install --cask alt-tab
-# brew install --cask betterzip
-brew install --cask ubar
-
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
